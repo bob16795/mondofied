c982d03d235136e55479b68a5beddd20f7beb3f2
bob16795 <prestosilver.ptp@gmail.com>
2018-12-26 12:04:04 -0500

    added more

--- bin/bar.sh
+++ bin/bar.sh
@@ -1,5 +1,5 @@
 while true;
 do
-	xsetroot -name "$(i3mpd) | $(i3battery BAT0) | $(i3mem) | $(i3pacman) | $(i3disk /) | $(i3internet) | $(i3mail)"
-    sleep 1
+	xsetroot -name "$(i3mpd) | $(i3battery BAT0) | $(i3mem) | $(i3pacman) | $(i3disk /) | $(i3mail)"
+    sleep .1
 done
