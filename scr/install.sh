a1384dd344ba6f68d2d93ed1e1fc8f94b8992547
bob16795 <prestosilver.ptp@gmail.com>
2019-01-03 14:57:19 -0500

    deleted some and made a theme
    Former-commit-id: 01a84990955e320a2ae61b1de257c22b22e1e079 [formerly 11fcbe31340fcb5781f5f1cf7c84c07bf3f6220f]
    Former-commit-id: 64eb4f229d577408b4cfd7f5d81bec62fd248849

--- scr/install.sh
+++ scr/install.sh
@@ -1,2 +1,6 @@
 sudo ln -fs ~/fon/scientifica/regular/scientifica-11.bdf /usr/share/fonts/scientifica-11.bdf
 sudo ln -fs ~/fon/scientifica/bold/scientifica-11.bdf /usr/share/fonts/scientificaBold-11.bdf
+
+sudo pacman -S pip
+pip install yay
+yay -S alacritty 
