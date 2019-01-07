fc6363b76c6e9e050a3c7a023c694b31a7044239
bob16795 <prestosilver.ptp@gmail.com>
2019-01-07 07:15:03 -0500

    Mon Jan  7 07:15:03 EST 2019 manual commit

--- .screenlayout/lol.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output VGA-1 --mode 1280x1024 --pos 2646x0 --rotate right --output LVDS-1 --primary --mode 1366x768 --pos 0x0 --rotate normal --output HDMI-3 --mode None --pos 1366x0 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-3 --off --output DP-2 --off --output DP-1 --off
